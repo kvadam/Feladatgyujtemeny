783867ae4fdcbfa1808aa59bb22bcc34a8523f1a
kvadam <kvadam@gmail.com>
2026-01-20 10:11:04 +0100

    Parancs kereső autatikusan végigkeresi a feladatok mapppa teljes tartalmát.

--- utasitasok/print_feladatokban.py
+++ utasitasok/print_feladatokban.py
@@ -1,12 +1,29 @@
+from pathlib import Path
+
+def keres(utvonal: Path, parancs: str) -> None:
+    for objektum in utvonal.iterdir():
+
+        if objektum.is_dir():
+            keres(objektum, parancs)
+
+        if objektum.name[-2:] == "py":
+            with open(objektum, "r", encoding="utf-8") as forras:
+                ssz = 1
+                for sor in forras:
+                    sor = sor.strip()
+                    if parancs in sor:
+                        print(f'File "{objektum}", line {ssz}')
+                        print(f"\t{sor}\n")
+                    ssz+=1
+
+
 def main() -> None:
-    eleresi_ut: str = "../feladatok/digitalis_kultura_erettsegi/emelt/2024maj/v1/belepteto.py"
-    parancsnev: str = "print"
-    with open(eleresi_ut, "r", encoding="utf-8") as forras:
-        ssz = 1
-        for sor in forras:
-            if parancsnev in sor.strip():
-                print(f'File "{eleresi_ut}", line {ssz}')
-            ssz+=1
+    projekt_mappa = Path(__file__).resolve().parent.parent
+    feladatok_mappa = projekt_mappa / "feladatok"
+
+    parancsnev: str = input("Milyen parancsot keresel?: ")
+    keres(feladatok_mappa, parancsnev)
+
 
 if __name__ == "__main__":
     main()

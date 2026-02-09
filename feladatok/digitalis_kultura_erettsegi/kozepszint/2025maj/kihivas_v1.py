058d7a08c52d3668b6b2aa351cb3cfdb9e7c11af
kvadam@gmail.com <kvadam@gmail.com>
2026-02-09 16:40:58 +0100

    Digitális kultúra középsz. 2025. május v1 egyszerűsítve.

--- feladatok/digitalis_kultura_erettsegi/kozepszint/2025maj/kihivas_v1.py
+++ feladatok/digitalis_kultura_erettsegi/kozepszint/2025maj/kihivas_v1.py
@@ -1,16 +1,25 @@
-tevekenysegek: dict[str, int] = {"U": 1, "G": 1, "F": 2, "K": 10}
-
 print("1. feladat")
 aktivitas: str = input("Adja meg az aktivitását: ")
 
 print("2. feladat")
-elert_tav: int = 0
+u: int = 0
+g: int = 0
+f: int = 0
+k: int = 0
 for a in aktivitas:
-    elert_tav += tevekenysegek[a]
+    if a == "U":
+        u += 1
+    elif a == "G":
+        g += 1
+    elif a == "F":
+        f += 2
+    elif a == "K":
+        k += 10
+elert_tav: int = u + g + f + k
 print(f"Az elért távolság: {elert_tav}.")
 
 print("3. feladat")
-if len(set(aktivitas)) == 4:
+if u > 0 and g > 0 and f > 0 and k > 0:
     print("Bravó! a jutalma még 10 km.")
     elert_tav += 10
 else:

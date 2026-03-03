beb5bce1f2e53e4fd6847ac8d7fa8651d4edfef4
kvadam@gmail.com <kvadam@gmail.com>
2026-03-03 21:18:03 +0100

    Kódjavítás feladat módosítva.

--- feladatok/gyakorlatok/hibakereses/feladat1.py
+++ feladatok/gyakorlatok/hibakereses/feladat1.py
@@ -1,4 +1,25 @@
 """
+arak: list[str] = [200, 300, 'négyszáz', 500]
+kredit: int = float(input("Add meg mennyi kredited van: ")
+szallitasi_koltseg: int = 300.0
 
+osszar += 0
+for ar in range(arak):
+    osszar -= arak
+
+vegrehajthato = true
+
+if osszarak > 1000
+    vegrehajthato == False
+    print(A megrendelések értéke nem éri el az 1000 kreditet.)
+if kredit < osszar * szallitasi_koltseg:
+    print{"Nincs elég kredit a vásárláshoz."]
+    vegrehajt = True
+if vegrehajthato = True:
+    if osszes > 5000:
+        szallitasi_koltseg = -300
+    husegpontok: int = osszar / 100
+    kredit += osszar + szallitasi_koltseg - husegpontok
+    husegpontok = kredit
+    print("Fennmaradó kreditek: {kredit}, fennmaradó hűségpontok: {husegpontok}")
 """
-
---
+++ feladatok/gyakorlatok/hibakereses/feladat1_megoldas.py
@@ -0,0 +1,51 @@
+"""
+A feladat 27 hibát tartalmaz, elhelyezkedésük ( <- hibák száma a sorban):
+
+arak: list[str] = [200, 300, 'négyszáz', 500] <- 2
+kredit: int = float(input("Add meg mennyi kredited van: ") <- 2
+szallitasi_koltseg: int = 300.0 <- 1
+
+osszar += 0 <- 1
+for ar in range(arak): <- 1
+    osszar -= arak <- 2
+
+vegrehajthato = true <- 1
+
+if osszarak > 1000 <- 3
+    vegrehajthato == False <- 1
+    print(A megrendelések értéke nem éri el az 1000 kreditet.) <- 1
+if kredit < osszar * szallitasi_koltseg: <- 1
+    print{"Nincs elég kredit a vásárláshoz."] <- 2
+    vegrehajt = True <- 2
+if vegrehajthato = True: <- 1
+    if osszes > 5000: <- 1
+        szallitasi_koltseg = -300 <- 1
+    husegpontok: int = osszar / 100 <- 1
+    kredit += osszar + szallitasi_koltseg - husegpontok <- 1
+    husegpontok = kredit <- 1
+    print("Fennmaradó kreditek: {kredit}, fennmaradó hűségpontok: {husegpontok}")  <- 1
+"""
+
+arak: list[int] = [200, 300, 400, 500]
+kredit: int = int(input("Add meg mennyi kredited van: "))
+szallitasi_koltseg: int = 300
+
+osszar: int = 0
+for ar in arak:
+    osszar += ar
+
+vegrehajthato = True
+
+if osszar < 1000:
+    vegrehajthato = False
+    print("A megrendelések értéke nem éri el az 1000 kreditet.")
+if kredit < osszar + szallitasi_koltseg:
+    print("Nincs elég kredit a vásárláshoz.")
+    vegrehajthato = False
+if vegrehajthato:
+    if osszar > 5000:
+        szallitasi_koltseg = 0
+    husegpontok: int = osszar // 100
+    kredit -= osszar + szallitasi_koltseg - husegpontok
+    husegpontok = 0
+    print(f"Fennmaradó kreditek: {kredit}, fennmaradó hűségpontok: {husegpontok}")
